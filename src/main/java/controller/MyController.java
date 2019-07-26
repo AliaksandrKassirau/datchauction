9e1be1fee63ce1e950f3b11e8c2151faf36ac3e7
LogicAndPaws <48258882+LogicAndPaws@users.noreply.github.com>
2019-07-26 15:22:37 +0300

    Login and Registration pages templates

--- src/main/java/controller/MyController.java
+++ src/main/java/controller/MyController.java
@@ -1,7 +1,11 @@
 package controller;
+import entities.LoginUser;
+import entities.RegistrationUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyController {
@@ -12,5 +16,46 @@ public class MyController {
 
         return "index.html";
     }
+    @GetMapping("/login")
+    public String login(Model model) {
+        if(!model.containsAttribute("loginMessage")) {
+            LoginUser lu = new LoginUser();
+            model.addAttribute("loginMessage", "");
+            model.addAttribute("loginUser", lu);
+        }
+        return "login.html";
+    }
+    @GetMapping("/confirmLogin")
+    public String confirmLogin(@ModelAttribute("loginUser") LoginUser lu,Model model) {
+        //TODO check in base
+        if(lu.getEmail().equals("default@gmail.com") && lu.getPassword().equals("default")) {
+            model.addAttribute("username", "DefaultUser");
+            return "home.html";
+        }else{
+            model.addAttribute("loginMessage","Wrong email or password. Try again");
+            return "login.html";
+        }
+    }
+    @RequestMapping("/registration")
+    public String Registration(Model model){
+        RegistrationUser ru = new RegistrationUser();
+        model.addAttribute("regUser",ru);
+        return "registration.html";
+    }
 
+    private boolean isRegistrationValid(RegistrationUser ru){
+        if(!ru.getPassword().equals(ru.getConfirmPassword()))return false;
+        //TODO check nickname,email,passwordDifficult...
+        return true;
+    }
+
+    @RequestMapping("/confirmRegistration")
+    public String confirmRegistration(@ModelAttribute("regUser")RegistrationUser ru,Model model){
+        //TODO request registration
+        if(isRegistrationValid(ru)) {
+            model.addAttribute("username",ru.getUsername());
+            return "home.html";
+        }
+        return "registration.html";
+    }
 }
